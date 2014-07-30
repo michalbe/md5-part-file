53e250e21a09f165f6de5ffb0e4ab7bf225429bc
Michal Budzynski <michal@virtualdesign.pl>
2014-07-30 20:36:50 +0200

    [MD5-PART-FILE] - :dolls: readme update

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ module.exports = function(file, start, end, cb) {
   var buffer = new Buffer(length);
   fs.open(file, 'r', function(err, fd) {
     if (err){
-      console.log(err);
+      cb(err);
       return;
     }
     fs.read(fd, buffer, 0, length, 0, function(err, bufferLength) {
       if (err){
-        console.log(err);
+        cb(err);
         return;
       }
       md5sum.update(buffer.toString('utf-8', 0, bufferLength));
-      fs.close(fd, function(){
+      fs.close(fd, function() {
         cb(md5sum.digest('hex'));
       });
     });
