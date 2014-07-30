299683b67c39dc3539be576d17736a57f91b8a04
Michal Budzynski <michal@virtualdesign.pl>
2014-07-30 19:34:55 +0200

    [MD5-PART-FILE] - :car: open part of the file

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 'use strict';
+
+var md5 = require('md5-o-matic');
+var fs = require('fs');
+
+var md5pf = function(file, start, end, cb) {
+  var length = end - start;
+  var buffer = new Buffer(length);
+  fs.open(file, 'r', function(err, fd) {
+    if (err){
+      console.log(err);
+      return;
+    }
+    fs.read(fd, buffer, 0, length, 0, function(){
+      cb(md5(buffer.toString()));
+    });
+  });
+};
+
+
+md5pf('README.md', 0, 1023, function(hash){
+  console.log('hash', hash);
+});
